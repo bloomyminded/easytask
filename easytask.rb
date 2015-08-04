51a83b4538c4019c503b34e5bd87611042f68636
Cody Roberts <codyjroberts@gmail.com>
2015-08-04 13:51:50 -0500

    added task creation"

--- easytask.rb
+++ easytask.rb
@@ -1,7 +1,31 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'commander/import'
+require 'sequel'
 
 program :name, "Easy Task"
 program :version, '0.0.1'
 program :description, "CLI Task Manager"
+
+DB = Sequel.sqlite('tasks.db')
+
+unless DB.table_exists? :tasks
+  DB.create_table(:tasks) do
+    primary_key :id
+    String :description
+    Boolean :completed
+  end
+end
+
+tasks_db = DB[:tasks]
+
+command :create do |create|
+  create.syntax = "easytask create"
+  create.description = "Creates a task"
+  create.option "--description STRING", String, "Description"
+  create.action do |args, options|
+    options.description = ask("Task Description: ") unless options.description
+    tasks_db.insert(description: options.description, completed: false)
+    say "Task Created"
+  end
+end

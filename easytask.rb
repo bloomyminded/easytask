e7917fa16536ca0153ff5475d450ead91a95afbe
Cody Roberts <codyjroberts@gmail.com>
2015-08-04 15:21:12 -0500

    added remove

--- easytask.rb
+++ easytask.rb
@@ -17,7 +17,7 @@ unless DB.table_exists? :tasks
   end
 end
 
-tasks_db = DB[:tasks]
+tasks_dataset = DB[:tasks]
 
 command :create do |create|
   create.syntax = "easytask create"
@@ -25,7 +25,7 @@ command :create do |create|
   create.option "--description STRING", String, "Description"
   create.action do |args, options|
     options.description = ask("Task Description: ") unless options.description
-    tasks_db.insert(description: options.description, completed: false)
+    tasks_dataset.insert(description: options.description, completed: false)
     puts "Task Created"
   end
 end
@@ -34,8 +34,23 @@ command :list do |list|
   list.syntax = "easytask list"
   list.description = "List tasks"
   list.action do |args, options|
-    tasks_db.each do |task|
+    tasks_dataset.each do |task|
       puts "#{task[:id]}. #{task[:description]}"
+    end
+  end
+end
+
+command :remove do |remove|
+  remove.syntax = "easytask remove"
+  remove.description = "Remove tasks"
+  remove.action do |args, options|
+    tasks = tasks_dataset.map(:description)
+    if tasks.empty?
+      puts "No tasks to remove"
+    else
+      choice = choose("Task: ", tasks)
+      tasks_dataset.where(description: choice).delete
+      puts "Removed: #{choice}"
     end
   end
 end
